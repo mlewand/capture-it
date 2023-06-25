d1835b80bc2ef39defbccfa2dd199df6c702756e
Marek Lewandowski <code@mlewandowski.com>
2023-06-25 22:57:28 +0200

    Added a way to close the application.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
-import { app, BrowserWindow, globalShortcut, Tray } from 'electron';
+import { app, BrowserWindow, globalShortcut, Tray, Menu } from 'electron';
 import * as path from 'path';
-import * as url from 'url';
+import { CLIENT_RENEG_WINDOW } from 'tls';
 
-let mainWindow: Electron.BrowserWindow | null;
+class AppMainWindow extends BrowserWindow {
+  _finalClosing: boolean = false;
+
+  public forceClose() {
+    this._finalClosing = true;
+    this.emit('close', { defaultPrevented: false } );
+  }
+}
+
+let mainWindow: AppMainWindow | null;
 let tray: Tray | null;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
+  mainWindow = new AppMainWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -20,8 +29,9 @@ function createWindow() {
     mainWindow = null;
   });
 
-  mainWindow.on('close', (event) => {
-    if (mainWindow) {
+  mainWindow.on('close', ( event : any ) => {
+    if ( event && mainWindow && !mainWindow._finalClosing ) {
+
       event.preventDefault();
       mainWindow.hide();
     }
@@ -34,6 +44,20 @@ app.on('ready', () => {
   // Create system tray
   tray = new Tray(path.join(__dirname, '..', 'assets', 'icon.png'));
   tray.setToolTip('Electron app');
+
+  // Create context menu for the tray
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Close',
+      click: () => {
+        if (mainWindow) {
+          mainWindow.forceClose();
+          app.quit();
+        }
+      }
+    }
+  ]);
+  tray.setContextMenu(contextMenu);
 
   tray.on('click', () => {
     if (mainWindow) {
