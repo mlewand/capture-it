6b7d4c1d646b1c0809d5d65c8e9bb17dd6b40273
Marek Lewandowski <code@mlewandowski.com>
2023-07-05 20:41:07 +0200

    Moved note capture (uploading via API) logic to the main process.
    This cleans up the unmaintanable app.ts and puts more logical order (that's actually a command).

--- app/app.ts
+++ app/app.ts
@@ -48,92 +48,6 @@ function getActiveWorkspace(): WorkspaceInfo | undefined {
 	}
 }
 
-function appendParagraphToNotionPage(pageId: string, notionToken: string, paragraphText: string): Promise<void> {
-	const url = `https://api.notion.com/v1/blocks/${pageId}/children`;
-	const requestOptions: RequestInit = {
-		method: 'PATCH',
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${notionToken}`,
-			'Notion-Version': '2021-05-13' // Replace with the desired Notion API version
-		},
-		body: JSON.stringify({
-			children: [
-				{
-					object: 'block',
-					type: 'paragraph',
-					paragraph: {
-						text: [
-							{
-								type: 'text',
-								text: {
-									content: paragraphText
-								}
-							}
-						]
-					}
-				}
-			]
-		})
-	};
-
-	return fetch(url, requestOptions)
-		.then(async ( response ) => {
-			const data = await response.json() as any;
-
-			if ( !response.ok ) {
-				throw `Error (${ response.status }, ${ data.code }): ${ data.message }`;
-			}
-
-			return data;
-		} )
-		.then( data => {
-			console.log( 'Paragraph appended successfully:', data );
-
-			// Unfortunately this kind of operation doesn't return the page URL so we need to do it manually.
-			data.url = 'https://www.notion.so/' + pageId;
-
-			return data;
-		} );
-}
-
-async function appendPageToDatabase(databaseId: string, apiToken: string, pageText: string): Promise<void> {
-	const response = await fetch('https://api.notion.com/v1/pages', {
-		method: 'POST',
-		headers: {
-			'Authorization': `Bearer ${apiToken}`,
-			'Content-Type': 'application/json',
-			'Notion-Version': '2021-05-13'
-		},
-		body: JSON.stringify({
-			parent: {
-				database_id: databaseId
-			},
-			properties: {
-				Name: {
-					title: [
-						{
-							text: {
-								content: pageText
-							}
-						}
-					]
-				}
-			}
-		})
-	});
-
-	const data = await response.json();
-
-	if (response.ok) {
-		console.log('Page appended successfully:', data);
-	} else {
-		throw `Error (${ response.status }, ${ data.code }): ${ data.message }`;
-	}
-
-	return data;
-}
-
 async function asyncInitialization(): Promise<boolean> {
 	let containerToBeShown: HTMLElement | null = document.getElementById('app-tab');
 	let errorContent: string | null = null;
@@ -173,7 +87,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 	setupInitialFocus();
 
-	console.log('DOMContentLoaded: sending sample capture command');
+	console.log( 'DOMContentLoaded: sending sample capture command' );
 	electronBridge.promisedInvoke( 'executeCommandAsync', 'captureItem', 'lorem ipsum param' )
 		.then( res => console.log('DOMContentLoaded: RESOLVED', res ) )
 		.catch( err => console.log('DOMContentLoaded: REJECTED', err ) );
@@ -259,23 +173,7 @@ function setupInitialFocus(): void {
 }
 
 function submitNote( text: string, openPage = false ) : void {
-	const activeWorkspace = getActiveWorkspace();
-
-	if ( !activeWorkspace ) {
-		console.error( 'Configuration not loaded or no active workspace is set' );
-		return;
-	}
-
-	const { pageId, dataBaseId, notionToken } = activeWorkspace;
-
-	if ( text.trim() === '' ) {
-		console.warn( "Can't send empty item." );
-		return;
-	}
-
-	const insertPromise = dataBaseId ? appendPageToDatabase(dataBaseId, notionToken, text)
-		: appendParagraphToNotionPage(pageId!, notionToken, text);
-
+	const insertPromise = electronBridge.promisedInvoke( 'executeCommandAsync', 'captureItem', text );
 	const notification = addNotification( text, 'loading' );
 
 	insertPromise.then( (data: any) => {
--- src/Command/CaptureItem.ts
+++ src/Command/CaptureItem.ts
@@ -10,11 +10,109 @@ export default class CaptureItemCommand extends Command {
 	}
 
 	public async execute( noteText: string ): Promise<any> {
-		console.log( 'captureItem command executed' );
-		return new Promise( ( resolve, reject ) => {
-			setTimeout( () => {
-				resolve( { message: 'Hello from CaptureItemCommand', text: noteText } );
-			}, 500 );
-		} );
+		// const activeWorkspace = getActiveWorkspace();
+		const activeWorkspace = this.app.activeWorkspace;
+
+		if ( !activeWorkspace ) {
+			throw new Error( 'Configuration not loaded or no active workspace is set' );
+		}
+
+		const { pageId, dataBaseId, notionToken } = activeWorkspace;
+
+		if ( noteText.trim() === '' ) {
+			throw new Error( "Can't send an empty item." );
+		}
+
+		const insertPromise = dataBaseId ? appendPageToDatabase(dataBaseId, notionToken, noteText)
+			: appendParagraphToNotionPage(pageId!, notionToken, noteText);
+
+
+		return await insertPromise;
 	}
+}
+
+function appendParagraphToNotionPage(pageId: string, notionToken: string, paragraphText: string): Promise<void> {
+	const url = `https://api.notion.com/v1/blocks/${pageId}/children`;
+	const requestOptions: RequestInit = {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${notionToken}`,
+			'Notion-Version': '2021-05-13' // Replace with the desired Notion API version
+		},
+		body: JSON.stringify({
+			children: [
+				{
+					object: 'block',
+					type: 'paragraph',
+					paragraph: {
+						text: [
+							{
+								type: 'text',
+								text: {
+									content: paragraphText
+								}
+							}
+						]
+					}
+				}
+			]
+		})
+	};
+
+	return fetch(url, requestOptions)
+		.then(async ( response ) => {
+			const data = await response.json() as any;
+
+			if ( !response.ok ) {
+				throw `Error (${ response.status }, ${ data.code }): ${ data.message }`;
+			}
+
+			return data;
+		} )
+		.then( data => {
+			console.log( 'Paragraph appended successfully:', data );
+
+			// Unfortunately this kind of operation doesn't return the page URL so we need to do it manually.
+			data.url = 'https://www.notion.so/' + pageId;
+
+			return data;
+		} );
+}
+
+async function appendPageToDatabase(databaseId: string, apiToken: string, pageText: string): Promise<void> {
+	const response = await fetch('https://api.notion.com/v1/pages', {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${apiToken}`,
+			'Content-Type': 'application/json',
+			'Notion-Version': '2021-05-13'
+		},
+		body: JSON.stringify({
+			parent: {
+				database_id: databaseId
+			},
+			properties: {
+				Name: {
+					title: [
+						{
+							text: {
+								content: pageText
+							}
+						}
+					]
+				}
+			}
+		})
+	});
+
+	const data = await response.json();
+
+	if (response.ok) {
+		console.log('Page appended successfully:', data);
+	} else {
+		throw `Error (${ response.status }, ${ data.code }): ${ data.message }`;
+	}
+
+	return data;
 }
