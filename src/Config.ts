ea2a535169cbefd8e1f8835492e99a76d235a10e
Marek Lewandowski <code@mlewandowski.com>
2023-07-23 23:33:16 +0200

    Closes #20 - Added handling for config auto reload.

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import { homedir } from 'os';
-import { promises as fs } from 'fs';
+import { promises as fs, watch as watchFs } from 'fs';
+import { debounce } from 'lodash';
 
 import { EventEmitter } from 'events';
 
@@ -26,30 +27,30 @@ export default class Config extends EventEmitter implements ConfigFileInterface 
 	forceOpenLinksInNotionApp: boolean;
 	tags?: { [key: string]: string };
 
-	// @todo: config file could emit events when changed
-	constructor( options: ConfigFileInterface ) {
-		super();
-		this.workspaces = options.workspaces;
-		this.invocationHotKey = options.invocationHotKey;
-		this.forceOpenLinksInNotionApp = options.forceOpenLinksInNotionApp;
-		this.tags = options.tags;
-	}
+		constructor( options: ConfigFileInterface ) {
+			super();
+
+			// Can't use  this._fillFromObject(), TS isn't smart enough.
+			// this._fillFromObject( options );
+			this.workspaces = options.workspaces;
+			this.invocationHotKey = options.invocationHotKey;
+			this.forceOpenLinksInNotionApp = options.forceOpenLinksInNotionApp;
+			this.tags = options.tags;
+		}
 
 	public static async loadFromUserDirectory(): Promise<Config> {
-		const configPath = this.getUserConfigPath();
-		const stat = await fs.stat( configPath );
+		const filePath = Config.getUserConfigPath();
+		const ret = new Config( await Config._readFromFile( filePath ) );
 
-		if ( !stat.isFile() ) {
-			throw new Error( 'Configuration file not found in ' + configPath );
-		}
+		const debouncedListener = debounce( async () => {
+			console.log('DETECTED CONFIG CHANGE');
+			ret._fillFromObject( await Config._readFromFile( filePath ) );
+			ret.emit( 'changed' );
+		}, 700 );
 
-		const parsedData = JSON.parse( await fs.readFile(  configPath, 'utf-8' ) );
+		watchFs( filePath, { encoding: 'utf8' }, debouncedListener );
 
-		if ( !parsedData.workspaces ) {
-			throw new Error( 'Configuration file is missing workspaces' );
-		}
-
-		return new Config( parsedData );
+		return ret;
 	}
 
 	public static getUserConfigPath(): string {
@@ -63,6 +64,29 @@ export default class Config extends EventEmitter implements ConfigFileInterface 
 
 		// Update the file.
 		this._saveFile();
+	}
+
+	private static async _readFromFile( configPath : string ) {
+		const stat = await fs.stat( configPath );
+
+		if ( !stat.isFile() ) {
+			throw new Error( 'Configuration file not found in ' + configPath );
+		}
+
+		const parsedData = JSON.parse( await fs.readFile(  configPath, 'utf-8' ) );
+
+		if ( !parsedData.workspaces ) {
+			throw new Error( 'Configuration file is missing workspaces' );
+		}
+
+		return parsedData;
+	}
+
+	private async _fillFromObject( parsedData: ConfigFileInterface ) {
+		this.workspaces = parsedData.workspaces;
+		this.invocationHotKey = parsedData.invocationHotKey;
+		this.forceOpenLinksInNotionApp = parsedData.forceOpenLinksInNotionApp;
+		this.tags = parsedData.tags;
 	}
 
 	private _saveFile(): Promise<boolean> {
