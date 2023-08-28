d21eeb1aaf1779876c41bccd750463d070c36443
Marek Lewandowski <code@mlewandowski.com>
2023-08-28 13:37:29 +0200

    Fixed main ndistribution file paths.

--- packages/capture-it-cli/src/cli.ts
+++ packages/capture-it-cli/src/cli.ts
@@ -1,8 +1,8 @@
 
-// import { addNote } from 'capture-it-notes';
-import { NotesModule } from 'capture-it-notes';
+import { addNote } from 'capture-it-notes';
+// import { NotesModule } from 'capture-it-notes';
 
-const addNote = NotesModule.addNote;
+// const addNote = NotesModule.addNote;
 
 function main() {
 	console.log('test - working fine');
--- packages/capture-it-notes/src/index.ts
+++ packages/capture-it-notes/src/index.ts
@@ -1,4 +1,9 @@
 
 // export { addNote } from './notesmodule';
-export * as NotesModule from './notesmodule';
+// export * from './notesmodule';
 // export { dumpNoter } from './notesmodule';
+
+export function addNote( note: string ) {
+	console.log( 'eeeevil implementation' );
+	console.log( note );
+}
