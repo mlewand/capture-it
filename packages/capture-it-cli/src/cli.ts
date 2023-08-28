866a4471286293231d9c058cb0b887a8f2c0b2b4
Marek Lewandowski <code@mlewandowski.com>
2023-08-28 20:13:39 +0200

    CLI uses a shared Notion module.

--- packages/capture-it-cli/src/cli.ts
+++ packages/capture-it-cli/src/cli.ts
@@ -1,9 +1,19 @@
 
 import { addNote } from '@mlewand/capture-it-notes';
+import { WorkspaceInfo } from '@mlewand/capture-it-core';
+import * as CaptureItNotion from '@mlewand/capture-it-notion';
 
-function main() {
+const config = require( '/Users/mlewand/.remove-me-config.json' ).items as WorkspaceInfo[];
+
+async function main() {
 	console.log( 'test - working fine' );
 	addNote( 'stefanio' );
+	console.log( config );
+
+	console.log('adding');
+	CaptureItNotion.appendPageToDatabase( 'hello world from cli module', config[ 0 ], new Set<string>() )
+		.then( () => console.log( 'done' ) )
+		.catch( err => console.error( 'error', err ) );
 }
 
 main();
