c1a0e56d97d03800a6824617b381f2032f4d7a3c
Marek Lewandowski <code@mlewandowski.com>
2023-09-05 12:42:00 +0200

    Moved Redux provider to the App component. Adjusted test case..

--- packages/capture-it-desktop-app-frontend/src/App.tsx
+++ packages/capture-it-desktop-app-frontend/src/App.tsx
@@ -1,15 +1,36 @@
 import MissingConfigTab from './MissingConfigTab';
 import NoWorkspacesTab from './NoWorkspacesTab';
 import MainCaptureItTab from './MainCaptureItTab';
+import store from './store';
+import { Provider } from 'react-redux';
+import { useEffect } from 'react';
+
 import './App.css';
 
+interface ElectronBridge {
+  receive: ( channel: string, func: ( ...args: any[] ) => void ) => void;
+  invoke: ( channel: string, ...args: any[] ) => Promise<any>;
+  send: ( channel: string, ...args: any[] ) => void;
+  promisedInvoke: ( channel: string, ...args: any[] ) => Promise<any>;
+}
+
+const electronBridge: ElectronBridge = ( window as any ).electron;
+
 function App() {
+  useEffect( () => {
+    console.log('test log');
+
+    console.log('electron bridge:', electronBridge);
+
+    return () => { };
+  }, [] );
+
   return (
-    <>
+    <Provider store={store}>
       <MainCaptureItTab />
       <MissingConfigTab />
       <NoWorkspacesTab />
-    </>
+    </Provider>
   );
 }
 
